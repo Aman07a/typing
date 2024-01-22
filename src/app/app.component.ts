f3262f3eaa5e75babf65e58e0d9bf247673c3f39
Aman07a <aman16.aa17@gmail.com>
2024-01-22 16:32:18 +0100

    Displaying Content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
+
+lorem.sentence();
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'typing';
